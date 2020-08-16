69613166109b47646e9360f13c58e47323f7fced
maheshgurav <maheshgurav100@gmail.com>
2020-08-16 19:15:28 +0530

    Use of input function

--- basic_print.py
+++ basic_print.py
@@ -3,3 +3,11 @@ print("Hello world")
 print("Hello, I am {}".format("Sahil"))
 
 print("Hello, I am {FIRST_NAME} {LAST_NAME}".format(FIRST_NAME = "Sahil", LAST_NAME= "Jadhav"))
+
+print("Please insert first name")
+first_name = input()
+
+print("Please insert last name")
+last_name = input()
+
+print("Hello, I am {FIRST_NAME} {LAST_NAME}".format(FIRST_NAME = first_name, LAST_NAME= last_name))
